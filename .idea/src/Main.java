a3f9f118b9745055bffcd951d3665fbb9c644623
jmanrocks152 <jmanrocks152@gmail.com>
2017-05-22 20:17:34 -0500

    Starting code for the project

---
+++ .idea/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * Created by jmanrocks152 on 5/22/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+        int coinAmount;
+        for(int i = 0; i < testCases; i++) {
+
+        }
+
+    }
+}
+
